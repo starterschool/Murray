fd6817bc0a46985b89b32d6c76c3704a4f710e56
Sandor Weisz <santheo@mac.com>
2014-02-27 17:33:32 -0600

    Baddifying.

--- js/global.js
+++ js/global.js
@@ -1,4 +1,6 @@
 // Execute the following code when the page is loaded
 $(function () {
-
+    $('section .item').each(function(index, item) {
+        $(item).css('background-image', 'url(http://www.fillmurray.com/' + (250+index) + '/300)');
+    });
 });
