7863f191eb7154594c0c1f48a57c3d20866a0c8c
Sandor Weisz <santheo@mac.com>
2014-03-04 16:43:43 -0600

    Made left nav sticky.

--- js/global.js
+++ js/global.js
@@ -1,6 +1,16 @@
-// Execute the following code when the page is loaded
 $(function () {
+    // Mix up the Murray pics
     $('section .item').each(function(index, item) {
         $(item).css('background-image', 'url(http://www.fillmurray.com/' + (250+index*2) + '/300)');
+    });
+
+    // Make left nav sticky
+    var nav = $('nav');
+    var navTop = nav.offset().top;
+    var navLeft = nav.offset().left;
+    $(window).scroll(function() {
+        var makeItStick = navTop < $(window).scrollTop();
+        nav.toggleClass('stuck', makeItStick);
+        nav.css('left', makeItStick ? navLeft : 0);
     });
 });
